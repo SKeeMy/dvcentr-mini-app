840b4d0bebe49e2d6a5bafa83c170e9aa5c82003
Vyacheslav Kaplin <slavakaplin84@gmail.com>
2025-10-15 14:49:41 +1000

    git push origin master

--- src/components/ui/cart/cart.tsx
+++ src/components/ui/cart/cart.tsx
@@ -4,7 +4,7 @@ import { Cart as Icon } from '@/components/shared/icons/cart'
 import s from './cart.module.scss'
 export const Cart = () => {
   return (
-    <Link className={s.cart} href={'/cart'}>
+    <Link className={s.cart} href={'#'}>
         <Icon />
         <span className={s.cart_counter}>0</span>
     </Link>
--- src/components/ui/profile/profile.tsx
+++ src/components/ui/profile/profile.tsx
@@ -4,7 +4,7 @@ import { Profile as Icon } from '@/components/shared/icons/profile'
 import Link from 'next/link'
 export const Profile = () => {
   return (
-    <Link className={s.profile} href={'/profile'}>
+    <Link className={s.profile} href={'#'}>
         <Icon />
     </Link>
   )
