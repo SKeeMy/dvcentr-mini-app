9933ca7097eae57a1d95283bc34096b4d65a94ce
Vyacheslav Kaplin <slavakaplin84@gmail.com>
2025-10-23 11:29:07 +1000

    add popup

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,12 +6,13 @@ import { Cart } from '../shared/icons/cart'
 import s from './footer.module.scss'
 import { useCartStore } from '@/store/cart-store'
 import clsx from 'clsx'
-import { Cart as CartOrder } from '../cart/cart'
+
 import dynamic from 'next/dynamic'
 import { Trash } from '../shared/icons/trash'
 import { useEffect } from 'react'
 import { usePathname } from 'next/navigation'
 import { hapticFeedback } from '@telegram-apps/sdk'
+import { FooterPopup } from './footer-popup'
 const CartCounter = dynamic(() => import('../cart/cart-counter'), {
   ssr: false,
   loading: () => null
@@ -20,10 +21,7 @@ export const Footer = () => {
 
   const { openFooter, openFooterCart, closeFooterCart, items, clearCart } = useCartStore()
 
-  const handleRemoveItemsFromTrash = () => {
-    clearCart();
-    closeFooterCart();
-  }
+  
 
   useEffect(() => {
     if (items.length === 0) {
@@ -45,23 +43,21 @@ export const Footer = () => {
   if (pathname === '/') return null
   return (
 
-      <footer className={clsx(s.footer, openFooter && s.open)}>
-        {/* <Container className={s.footer_container}> */}
-        <div className={s.footer_content}>
-          <div className={clsx(s.footer_items, openFooter && items.length && s.footer_cart)}>
-            {!openFooter ? <button onClick={items.length > 0 ? openFooterCart : undefined} className={s.footer_item} >
-              <Cart />
-              <CartCounter />
-            </button> : <button onClick={closeFooterCart} className={s.footer_close_cart}>Закрыть</button>}
-            {openFooter && items.length > 0 && <button onClick={handleRemoveItemsFromTrash} className={s.footer_cart_trash}><Trash /></button>}
-          </div>
-          <div className={s.footer_body}>
-            <CartOrder />
-          </div>
-        </div>
+    <footer className={clsx(s.footer, openFooter && s.open)}>
+      {/* <Container className={s.footer_container}> */}
+      <div className={s.footer_content}>
+        <div className={clsx(s.footer_items, openFooter && items.length && s.footer_cart)}>
+          {!openFooter && <button onClick={items.length > 0 ? openFooterCart : undefined} className={s.footer_item} >
+            <Cart />
+            <CartCounter />
+          </button>}
 
-        {/* </Container> */}
-      </footer>
+        </div>
+        <FooterPopup />
+      </div>
+
+      {/* </Container> */}
+    </footer>
 
   )
 }
