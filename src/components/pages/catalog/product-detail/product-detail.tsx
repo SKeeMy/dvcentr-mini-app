de96f9aca3d255fcf6cb5824671371548deea53f
Vyacheslav Kaplin <slavakaplin84@gmail.com>
2025-10-31 16:14:28 +1000

    add product popup

--- src/components/pages/catalog/product-detail/product-detail.tsx
+++ src/components/pages/catalog/product-detail/product-detail.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import s from './product-detail.module.scss'
 import { useCartStore } from '@/store/cart-store'
 import { Container } from '@/components/container/container'
@@ -8,69 +8,96 @@ import Image from 'next/image'
 import { Zoom } from 'swiper/modules'
 import { formatPrice } from '@/app/utils/formatPrice'
 import clsx from 'clsx'
-
+import { Sheet, SheetRef } from 'react-modal-sheet'
+import { useFooterStore } from '@/store/footer-strore'
 export const ProductDetail = () => {
   const { currentProduct, addToCart, getItemQuantity } = useCartStore()
+  const { isOpen, closeFooter, contentType } = useFooterStore()
+  const [descriptionShow, setDescriptionShow] = useState(false)
   const quantityInCart = getItemQuantity(currentProduct.id)
   const isInCart = quantityInCart > 0
   const handleAddToCart = () => {
     addToCart(currentProduct)
   }
-  return (
-    <div>
-      <div className={s.product}>
-        <div className={s.product_images}>
-          <Swiper
-            className={s.swiper}
-            spaceBetween={10}
-            slidesPerView={1}
-            centeredSlides={true}
-            resistance={true}
-            speed={300}
-            zoom={{
-              maxRatio: 3,
-              minRatio: 1,
-            }}
-            modules={[Zoom]}
-          >
-            {currentProduct?.additional.images.map((src, idx) => (
-              <SwiperSlide key={idx} className={s.product_slide}>
-                <div className="swiper-zoom-container">
-                  <Image
-                    alt={currentProduct.title}
-                    src={src}
-                    width={600}
-                    height={600}
-                    quality={50}
-                    loading="lazy"
-                    className={s.image}
-                  />
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-        <div className={s.product_info}>
-          <Container className={s.container}>
-            <h3 className={s.product_title}>{currentProduct.title}</h3>
 
-            <div className={s.product_info_wrapper}>
-              <div className={s.product_info_top}>
-                <div dangerouslySetInnerHTML={{ __html: currentProduct.additional.description }} />
+  const ref = useRef<SheetRef>(null);
+  const snapPoints = [0, 1];
+
+
+ 
+
+
+  return (
+    <div className={s.product}>
+      <span onClick={() => setDescriptionShow(true)} className={s.btn}>Описание</span>
+      <div className={s.product_images}>
+        <Swiper
+          className={s.swiper}
+          spaceBetween={10}
+          slidesPerView={1}
+          centeredSlides={true}
+          resistance={true}
+          speed={300}
+          zoom={{
+            maxRatio: 3,
+            minRatio: 1,
+          }}
+          modules={[Zoom]}
+        >
+          {currentProduct?.additional.images.map((src, idx) => (
+            <SwiperSlide key={idx} className={s.product_slide}>
+              <div className="swiper-zoom-container">
+                <Image
+                  alt={currentProduct.title}
+                  src={src}
+                  width={600}
+                  height={600}
+                  quality={50}
+                  loading="lazy"
+                  className={s.image}
+                />
               </div>
-              <div className={s.product_info_bottom}>
-                <div className={s.product_info_bottom_price}>
-                  Цена:
-                  <span>{formatPrice(currentProduct.price)}</span>
-                </div>
-                <button onClick={handleAddToCart} disabled={isInCart} className={clsx(s.product_info_bottom_cart,)}>
-                  {isInCart ? 'Добавлен' : 'В корзину'}
-                </button>
-              </div>
-            </div>
-          </Container>
-        </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
+      <Sheet
+        isOpen={descriptionShow}
+        onClose={() => setDescriptionShow(false)}
+        // unstyled
+        ref={ref}
+        snapPoints={snapPoints}
+        initialSnap={1}
+        tweenConfig={{ ease: 'easeOut', duration: 0.3 }}
+        disableDrag={false}
+        modalEffectRootId="root"
+        dragCloseThreshold={0.4}
+        dragVelocityThreshold={200}>
+        <Sheet.Container className={s.product_info}>
+          {/* <h3 className={s.product_title}>{currentProduct.title}</h3> */}
+
+          <div className={s.product_info_wrapper}>
+
+            <Sheet.Header className={s.header} disableDrag={false} ><span onClick={() => setDescriptionShow(false)} className={s.header_close}>Закрыть</span></Sheet.Header>
+
+            <div className={s.product_info_top}>
+              <div dangerouslySetInnerHTML={{ __html: currentProduct.additional.description }} />
+            </div>
+
+          </div>
+        </Sheet.Container>
+      </Sheet>
+
+      <div className={s.product_info_bottom}>
+        <div className={s.product_info_bottom_price}>
+          Цена:
+          <span>{formatPrice(currentProduct.price)}</span>
+        </div>
+        <button onClick={handleAddToCart} disabled={isInCart} className={clsx(s.product_info_bottom_cart,)}>
+          {isInCart ? 'Добавлен' : 'В корзину'}
+        </button>
+      </div>
+
     </div>
 
   )
