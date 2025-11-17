7daeef658755ff77e00f01afd9b420daefd59be5
Vyacheslav Kaplin <slavakaplin84@gmail.com>
2025-11-17 10:00:20 +1000

    improve game

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,11 +1,36 @@
 'use client'
 import { ConcreteMixerGame } from "@/components/game/ConcreteMixerGame"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { StartGame } from "@/components/game/start-game"
+import { useRouter } from "next/router"
+import { isTMA } from "@telegram-apps/sdk"
+import { useAppBackButton } from "../hooks/useAppBackButton"
 
 export default function GameSection() {
   const [gameStarted, setGameStarted] = useState<boolean>(false)
+  const router = useRouter();
+  const { showButton, hideButton } = useAppBackButton(() => {
+    router.push('/');
+  });
 
+  // Инициализация кнопки назад - ТОЛЬКО ПРИ МОНТИРОВАНИИ
+  useEffect(() => {
+    const initBackButton = async () => {
+      try {
+        if (await isTMA()) {
+          showButton();
+        }
+      } catch (error) {
+        console.error('Ошибка инициализации кнопки назад:', error);
+      }
+    };
+
+    initBackButton();
+
+    return () => {
+      hideButton();
+    };
+  }, []); 
   return (
     <div style={{ height: '100vh', maxHeight: '700px' }}>
       {gameStarted ?
