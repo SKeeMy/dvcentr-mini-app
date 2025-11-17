c29474086ad237ff814c16ae23609c785e1d9f84
Vyacheslav Kaplin <slavakaplin84@gmail.com>
2025-11-17 10:54:18 +1000

    fix

--- src/components/get-remains/get-remains.tsx
+++ src/components/get-remains/get-remains.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx'
 import { Remains } from '../pages/home/remains/remains'
 import { useAuthStore } from '@/store/auth-store'
 import { useRemainsStore } from '@/store/remains-store'
+import { Box } from '../shared/box/box'
 
 export const GetRemains = () => {
   const { user } = useAuthStore()
@@ -19,6 +20,9 @@ export const GetRemains = () => {
           loading={isLoading}
         />
       ))}
+      {!data && <Box>
+          <h3>У вас нет остатков</h3>
+        </Box>}
 
       {data === null && isLoading &&
         <Remains
