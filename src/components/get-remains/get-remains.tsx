6c61b697ae6af2ec635161dc3e8c185c36106fb2
Vyacheslav Kaplin <slavakaplin84@gmail.com>
2025-11-17 10:37:12 +1000

    fix

--- src/components/get-remains/get-remains.tsx
+++ src/components/get-remains/get-remains.tsx
@@ -11,20 +11,6 @@ export const GetRemains = () => {
   return (
     <div className='popup_inner'>
       <h3 style={{textAlign: 'center'}}>Мои остатки</h3>
-      <div className='popup_content_wrapper'>
-        <div className="popup__data">
-          <div>
-            <span className="popup__desc">
-              {user?.phone || 'Номер не получен'}
-            </span>
-            <p className='popup__title'>Телефон</p>
-          </div>
-          {data && <div>
-            <span className="popup__desc">{data.DATA.Data.length}</span>
-            <p className='popup__title'>Позиций</p>
-          </div>}
-        </div>
-      </div>
       {data && data.DATA.Data.map((remain, index) => (
         <Remains
           key={`${remain.ItemId}-${remain.Company}-${index}`}
